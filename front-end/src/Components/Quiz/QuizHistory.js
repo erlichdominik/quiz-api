c73d45566fefc82e1102aa0201d5d1f1851be94f
MTomys <michalltomys@protonmail.ch>
2022-11-20 15:38:06 +0100

    add initial quiz history component

--- front-end/src/Components/Quiz/QuizHistory.js
+++ front-end/src/Components/Quiz/QuizHistory.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { mockQuizHistoryItems } from '../../utils/mock-data/mock-quiz-history';
 
 const QuizHistory = () => {
-  return <div>QuizHistory</div>;
+  const [quizHistoryItems, setQuizHistoryItems] = useState([]);
+
+  useEffect(() => {
+    // simulate getting data from api
+    setQuizHistoryItems((quizHistoryItems) => [...mockQuizHistoryItems]);
+  }, []);
+
+  return (
+    <div className="text-xl">
+      QuizHistory
+      <table className="table-auto border border-slate-400 border-spacing-3 border-separate">
+        <thead>
+          <tr>
+            <th>Quiz name</th>
+            <th>Date of completion</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {quizHistoryItems.map((item) => (
+            <tr key={item.id}>
+              <td>{item.quizName}</td>
+              <td>{item.completionDate.toLocaleString()}</td>
+              <td>{item.accuracy}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default QuizHistory;
---
+++ front-end/src/Utils/Mock-data/mock-quiz-history.js
@@ -0,0 +1,33 @@
+const dates = [
+  new Date('November 15, 2022, 14:25:07'),
+  new Date('November 16, 2022, 7:28:05'),
+  new Date('November 17, 2022, 19:27:21'),
+  new Date('November 12, 2022, 11:15:00'),
+];
+
+export const mockQuizHistoryItems = [
+  {
+    id: 1,
+    quizName: 'quiz',
+    completionDate: dates[0],
+    accuracy: 0.75728,
+  },
+  {
+    id: 2,
+    quizName: 'quiz',
+    completionDate: dates[1],
+    accuracy: 0.71788,
+  },
+  {
+    id: 3,
+    quizName: 'quiz',
+    completionDate: dates[2],
+    accuracy: 0.82784,
+  },
+  {
+    id: 4,
+    quizName: 'quiz',
+    completionDate: dates[3],
+    accuracy: 0.45781,
+  },
+];
