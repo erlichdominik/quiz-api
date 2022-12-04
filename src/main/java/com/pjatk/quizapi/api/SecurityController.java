f3dd8c54b422a3bab587f7a8e7dc080e7c9d24ad
DominikErlich <dominik.erlich@bnpps.pl>
2022-12-04 22:04:15 +0100

    change register to post

--- src/main/java/com/pjatk/quizapi/api/SecurityController.java
+++ src/main/java/com/pjatk/quizapi/api/SecurityController.java
@@ -12,7 +12,6 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -54,7 +53,7 @@ class SecurityController {
         }
     }
 
-    @PutMapping("/auth/register")
+    @PostMapping("/auth/register")
     public ResponseEntity<Boolean> register(@RequestBody @Valid AuthRequest request) {
         userService.createNewUser(request);
         return ResponseEntity.ok().build();
