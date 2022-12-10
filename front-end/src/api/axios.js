94ea10c9244095130c2dd1d584528d8dd70eec00
DominikErlich <dominik.erlich@bnpps.pl>
2022-12-10 16:41:57 +0100

    add correct proxy adress in package.json xD

--- front-end/src/api/axios.js
+++ front-end/src/api/axios.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'http://localhost:8080/';
 
 export default axios.create({
-  baseURL: BASE_URL,
+  // baseURL: BASE_URL,
+  proxy: {
+    host: '127.0.0.1:8080',
+  }
 });
 
 export const axiosPrivate = axios.create({
