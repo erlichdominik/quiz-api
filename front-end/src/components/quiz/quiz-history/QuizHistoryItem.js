86df01fa213e299390e5d78112b5791c0703fc83
MTomys <michalltomys@protonmail.ch>
2023-01-16 22:49:21 +0100

    quiz history date fix

--- front-end/src/components/quiz/quiz-history/QuizHistoryItem.js
+++ front-end/src/components/quiz/quiz-history/QuizHistoryItem.js
@@ -3,13 +3,11 @@ import React from "react";
 const percentageFormatter = (percentageValue) =>
   `${(percentageValue * 100).toFixed(2)}%`;
 
-const getDateString = (dateObj) => dateObj?.toISOString().split("T")[0];
-
 const QuizHistoryItem = ({ id, quizName, completionDate, pathways }) => {
   return (
     <>
       <div className="self-center">{quizName}</div>
-      <div className="self-center">{getDateString(completionDate)}</div>
+      <div className="self-center">{completionDate}</div>
       <div>
         {pathways.map((pathway) => (
           <>
