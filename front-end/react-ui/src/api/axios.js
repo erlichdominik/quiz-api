c84cbbc651a9447bb5ca1f36f66c204521f498c1
erlichdominik <dominik.erlich@bnpps.pl>
2023-01-17 21:51:07 +0100

    add changes after dep

--- front-end/react-ui/src/api/axios.js
+++ front-end/react-ui/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://secret-thicket-39797.herokuapp.com/';
 
 export default axios.create({
-  // baseURL: BASE_URL,
+  baseURL: BASE_URL,
   // proxy: {
   //   host: '127.0.0.1:8080',
   // }
