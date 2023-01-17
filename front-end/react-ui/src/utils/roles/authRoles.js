84c2e1a851e64a1310ce334ad147e0ccfbfce8b4
erlichdominik <dominik.erlich@bnpps.pl>
2023-01-17 21:29:36 +0100

    add all

--- front-end/react-ui/src/utils/roles/authRoles.js
+++ front-end/react-ui/src/utils/roles/authRoles.js
@@ -1,7 +1,12 @@
 const USER_ROLES = {
+<<<<<<< HEAD
   regularUser: "STUDENT",
   teacherUser: "TEACHER",
   adminUser: "ADMIN",
+=======
+  regularUser: 'USER',
+  adminUser: 'ADMIN',
+>>>>>>> 74faa1b (add all)
 };
 
 export default USER_ROLES;
