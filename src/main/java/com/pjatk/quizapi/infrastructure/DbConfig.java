0e25ec2b3e7608ef3740ce987be334197c0ade9e
erlichdominik <dominik.erlich@bnpps.pl>
2023-01-17 16:29:58 +0100

    add: db config

---
+++ src/main/java/com/pjatk/quizapi/infrastructure/DbConfig.java
@@ -0,0 +1,23 @@
+package com.pjatk.quizapi.infrastructure;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+class DbConfig {
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
